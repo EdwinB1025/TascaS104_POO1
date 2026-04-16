e1219e5cf37d5e60fb4b1f1396ea779ff20790cb
Edwin <edwin.barrera1025@gmail.com>
2026-04-16 12:19:32 +0200

    nivel2 completado

--- nivel2/PokerDice.php
+++ nivel2/PokerDice.php
@@ -2,38 +2,106 @@
 class PokerDice
 {
     public const array DADO = ["A", "K", "Q", "J", "7", "8"];
-    public ?int $dado1;
-    public ?int $dado2;
-    public ?int $dado3;
-    public ?int $dado4;
-    public ?int $dado5;
-    public ?int $dado6;
     public array $dados;
+    public array $dadosUltima;
+    public array $tiradas;
 
-    public function __construct($dado = null)
+    public function __construct(?int $dado = null)
     {
-        $this->dado1 = $dado;
-        $this->dado2 = $dado;
-        $this->dado3 = $dado;
-        $this->dado4 = $dado;
-        $this->dado5 = $dado;
-        $this->dado6 = $dado;
-        $this->dados = [$this->dado1, $this->dado2, $this->dado3, $this->dado4, $this->dado5, $this->dado6];
+        $this->dados = array_fill(0, 6, $dado);
+        $this->dadosUltima = array_fill(0, 6, $dado);
+        $this->tiradas = array_fill(0, 6, 0);
     }
 
-    public function tirarDado(): array
+    private function defaultIndex(int $n): int
     {
-        foreach ($this->dados as &$dado) {
+        $n = match (true) {
+            $n < 0 => 0,
+            $n > 5 => 5,
+            default => $n
+        };
+        return $n;
+    }
+
+    public function tirarDado(?int $n = null): void
+    {
+        $n = $n ?? 1;
+        $n = $this->defaultIndex($n - 1);
+        $this->dadosUltima[$n] = $this->dados[$n];
+        $this->dados[$n] = mt_rand(0, 5);
+        $this->tiradas[$n]++;
+    }
+    public function tirarDados(): void
+    {
+
+        $this->dadosUltima = $this->dados;
+        foreach ($this->dados as $i => &$dado) {
             $dado = mt_rand(0, 5);
+            $this->tiradas[$i]++;
         }
-        return $this->dados;
     }
 
-    public function mostrarLado(): void
+    public function mostrarUltimosDados(): string
     {
-        echo "Los dados son: \n";
-        foreach ($this->dados as $dado) {
-            echo self::DADO[$dado] . "\n";
+        $salida = "Los ultimos dados lanzados son: \n";
+        foreach ($this->dadosUltima as $i => $dado) {
+            $valor = match (true) {
+                $dado === null => "No se ha tirado de nuevo",
+                default => self::DADO[$dado]
+            };
+            $salida .= "El ultimo dado " . ($i + 1) . " es: $valor\n";
         }
+        return $salida;
+    }
+    public function mostrarDados(): string
+    {
+        $salida = "Los dados lanzados actualmente son: \n";
+        foreach ($this->dados as $i => $dado) {
+            $valor = match (true) {
+                $dado === null => "No se ha tirado",
+                default => self::DADO[$dado]
+            };
+            $salida .= "El dado " . ($i + 1) . " es: $valor\n";
+        }
+        return $salida;
+    }
+
+    public function mostrarUltimoDado(?int $n = null): string
+    {
+        $n = $n ?? 1;
+        $n = $this->defaultIndex($n - 1);
+        $dado = $this->dadosUltima[$n];
+        $valor = match (true) {
+            $dado === null => "No se ha tirado de nuevo",
+            default => self::DADO[$dado]
+        };
+        return "El ultimo dado " . ($n + 1) . " es: $valor\n";
+    }
+
+    public function mostrarDado(?int $n = null): string
+    {
+        $n = $n ?? 1;
+        $n = $this->defaultIndex($n - 1);
+        $dado = $this->dados[$n];
+        $valor = match (true) {
+            $dado === null => "No se ha tirado",
+            default => self::DADO[$dado]
+        };
+        return "El dado " . ($n + 1) . " es: $valor\n";
+    }
+    public function mostrarTirada(?int $n = null): string
+    {
+        $n = $n ?? 1;
+        $n = $this->defaultIndex($n - 1);
+        $salida = "El dado " . ($n + 1) . " se ha lanzado {$this->tiradas[$n]} veces.\n";
+        return $salida;
+    }
+    public function mostrarTiradas(): string
+    {
+        $salida = "Lanzamientos de dados: \n";
+        foreach ($this->tiradas as $i => $tirada) {
+            $salida .= "El dado " . ($i + 1) . "se ha lanzado $tirada veces.\n";
+        }
+        return $salida;
     }
 }
--- nivel2/index.php
+++ nivel2/index.php
@@ -4,5 +4,53 @@ spl_autoload_register(function ($class_name) {
 });
 
 $dado = new PokerDice();
+
+echo "\n-------LANZO EL PRIMER DADO---------\n";
+
+$dado->tirarDado(1);
+echo $dado->mostrarDado(1);
+echo $dado->mostrarUltimoDado(1);
+
+echo "\n-------LANZO EL TERCER DADO---------\n";
+
+$dado->tirarDado(3);
+echo $dado->mostrarDado(3);
+echo $dado->mostrarUltimoDado(3);
+
+
+echo $dado->mostrarUltimosDados();
+
+echo "\n-------MUESTRO LOS DADOS ACTUALES---------\n";
+
+echo $dado->mostrarDados();
+
+echo "\n-------LANZO EL PRIMER DADO DE NUEVO---------\n";
+
 $dado->tirarDado();
-$dado->mostrarLado();
+
+echo $dado->mostrarDados();
+echo $dado->mostrarUltimosDados();
+
+echo $dado->mostrarTirada();
+echo $dado->mostrarTiradas();
+
+echo "\n-------TIRO TODOS LOS DADOS---------\n";
+
+$dado->tirarDados();
+
+echo $dado->mostrarDados();
+
+echo "\n-------MUESTRO SEXTO DADO---------\n";
+
+echo $dado->mostrarDado(10);
+echo $dado->mostrarUltimosDados();
+echo $dado->mostrarTiradas();
+
+
+
+echo "\n-------TIRO TODOS LOS DADOS---------\n";
+
+$dado->tirarDados();
+echo $dado->mostrarDados();
+echo $dado->mostrarUltimosDados();
+echo $dado->mostrarTiradas();
