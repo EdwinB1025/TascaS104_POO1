816414fc30d4b366da74ad1f57c8c742ad1cca7c
Edwin <edwin.barrera1025@gmail.com>
2026-04-22 12:01:35 +0200

    nivel2 simplificado

--- nivel2/PokerDice.php
+++ nivel2/PokerDice.php
@@ -3,81 +3,56 @@ class PokerDice
 {
     public const array DADO = ["A", "K", "Q", "J", "7", "8"];
     public array $dados;
-    public array $dadosUltima;
     public array $tiradas;
 
     public function __construct(?int $dado = null)
     {
-        $this->dados = array_fill(0, 6, $dado);
-        $this->dadosUltima = array_fill(0, 6, $dado);
-        $this->tiradas = array_fill(0, 6, 0);
+        $this->dados = [];
+        $this->tiradas = array_fill(0, 5, 0);
     }
 
     private function defaultIndex(int $n): int
     {
         $n = match (true) {
-            $n < 0 => 0,
-            $n > 5 => 5,
+            $n < 1 => 1,
+            $n > 6 => 6,
             default => $n
         };
         return $n;
     }
 
-    public function tirarDados(?int $n = null): void
+    public function tirarDado(): int
     {
-        if ($n === null) {
-            $this->dadosUltima = $this->dados;
-            foreach ($this->dados as $i => &$dado) {
-                $dado = mt_rand(0, 5);
-                $this->tiradas[$i]++;
-            }
-        } else {
-            $n = $this->defaultIndex($n - 1);
-            $this->dadosUltima[$n] = $this->dados[$n];
-            $this->dados[$n] = mt_rand(0, 5);
-            $this->tiradas[$n]++;
+        return mt_rand(0, 5);
+    }
+
+    public function tirarDados(int $n): void
+    {
+        $n = $this->defaultIndex($n);
+        for ($i = 0; $i < $n; $i++) {
+            $this->dados[$i] = $this->tirarDado();
+            $this->tiradas[$i]++;
         }
     }
 
-    public function mostrarDados(?int $n = null, int $lag = 0): string
+    public function mostrarDados(): string
     {
-        $array = $lag === -1 ? $this->dadosUltima : $this->dados;
-        $palabras = $lag === -1 ? "ultimos" : "nuevos";
-        $palabra1 = $lag === -1 ? "ultimo" : "nuevo";
-        $palabra2 = $lag === -1 ? "por segunda vez" : "por primera vez";
-        if ($n === null) {
-            $salida = "Los $palabras dados lanzados son: \n";
-            foreach ($array as $i => $dado) {
-                $valor = match (true) {
-                    $dado === null => "No se ha tirado de nuevo $palabra2",
-                    default => self::DADO[$dado]
-                };
-                $salida .= "El $palabra1 dado " . ($i + 1) . " es: $valor\n";
-            }
-        } else {
-            $n = $this->defaultIndex($n - 1);
-            $dado = $array[$n];
-            $valor = match (true) {
-                $dado === null => "No se ha tirado $palabra2",
-                default => self::DADO[$dado]
-            };
-            return "El $palabra1 dado " . ($n + 1) . " es: $valor\n";
+        $salida = "Los ultimos dados lanzados son:\n";
+        foreach ($this->dados as $i => $dado) {
+            $poker = self::DADO[$dado];
+            $numero = $i + 1;
+            $salida .= "  Dado numero $numero es: $poker\n";
         }
-        return $salida;
+        return $salida . "\n";
     }
 
-    public function mostrarTiradas(?int $n = null): string
+    public function mostrarTiradas(): string
     {
-        if ($n === null) {
-            $salida = "Lanzamientos de dados: \n";
-            foreach ($this->tiradas as $i => $tirada) {
-                $salida .= "El dado " . ($i + 1) . "se ha lanzado $tirada veces.\n";
-            }
-        } else {
-            $n = $this->defaultIndex($n - 1);
-            $salida = "El dado " . ($n + 1) . " se ha lanzado {$this->tiradas[$n]} veces.\n";
-            return $salida;
+        $salida = "Los ultimos tiradas de  los dados son:\n";
+        foreach ($this->tiradas as $i => $tirada) {
+            $numero = $i + 1;
+            $salida .= "  Dado numero $numero se ha tirado: $tirada veces\n";
         }
-        return $salida;
+        return $salida . "\n";
     }
 }
--- nivel2/index.php
+++ nivel2/index.php
@@ -8,46 +8,11 @@ $dado = new PokerDice();
 echo "\n-------LANZO EL PRIMER DADO---------\n";
 
 $dado->tirarDados(1);
-echo $dado->mostrarDados(1);
-echo $dado->mostrarDados(1, -1);
-
-echo "\n-------LANZO EL TERCER DADO---------\n";
-
-$dado->tirarDados(3);
-echo $dado->mostrarDados(3);
-echo $dado->mostrarDados(3, -1);
-echo $dado->mostrarDados(null, -1);
-
-echo "\n-------MUESTRO LOS DADOS ACTUALES---------\n";
-
 echo $dado->mostrarDados();
-
-echo "\n-------LANZO EL PRIMER DADO DE NUEVO---------\n";
-
-$dado->tirarDados(-1);
-echo $dado->mostrarDados();
-echo $dado->mostrarDados(null, -1);
-
-echo $dado->mostrarTiradas(1);
 echo $dado->mostrarTiradas();
 
-echo "\n-------TIRO TODOS LOS DADOS---------\n";
+echo "\n-------LANZO 5 DADOS---------\n";
 
-$dado->tirarDados();
-
+$dado->tirarDados(5);
 echo $dado->mostrarDados();
-
-echo "\n-------MUESTRO SEXTO DADO---------\n";
-
-echo $dado->mostrarDados(10);
-echo $dado->mostrarDados(null, -1);
-echo $dado->mostrarTiradas();
-
-
-
-echo "\n-------TIRO TODOS LOS DADOS---------\n";
-
-$dado->tirarDados();
-echo $dado->mostrarDados();
-echo $dado->mostrarDados(null, -1);
 echo $dado->mostrarTiradas();
